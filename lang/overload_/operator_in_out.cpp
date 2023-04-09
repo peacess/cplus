1d67d689b8f2d5ac4d13e848ee1307ee25ed630e
Peace <peacess@163.com>
2023-04-09 23:15:43 +0800

    remove const

--- lang/overload_/operator_in_out.cpp
+++ lang/overload_/operator_in_out.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
+#include <string>
 
 /// @brief 好多c++的书都建议 operator >> 或 << 使用friend函数， 但在这里建议使用public memeber与全局operator
 class Data
 {
+    private:
+    int t;
+    std::string str;
 public:
-    inline std::istream &in(std::istream &cin) const
+    inline std::istream &in(std::istream &cin)
     {
+        cin >> t >> str;
         return cin;
     }
 
     inline std::ostream &out(std::ostream &cout) const
     {
+        cout << t << str;
         return cout;
     }
 };
 
-std::istream &operator>>(std::istream &cin, const Data &data)
+std::istream &operator>>(std::istream &cin, Data &data)
 {
     return data.in(cin);
 }
